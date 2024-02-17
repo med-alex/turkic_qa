dd8274298a34c3571ed3c0724b6aeb5be5994a2c
Alexandr Medovikov <medovikov-aleksandr66@mail.ru>
2024-02-17 16:13:21 +0500

    addition for previous commit

--- scripts/functions/preprocessing.py
+++ scripts/functions/preprocessing.py
@@ -1,4 +1,4 @@
-def insert_spans(text, answer_start_num, suggested_answer):
+def insert_spans(text, answer_start_num, suggested_answer, span_left, span_right):
     
     try: 
         isinstance(int(suggested_answer), int)
@@ -7,18 +7,20 @@ def insert_spans(text, answer_start_num, suggested_answer):
         if text[answer_start_num:answer_end_num] == \
             suggested_answer:
             text = text[:answer_start_num] + \
-                    '[' + suggested_answer + ']' + \
+                    span_left + suggested_answer + span_right + \
                     text[answer_end_num:]
                     
     return text
 
 
-def get_data_with_spans(data):
+def get_data_with_spans(data, span_left, span_right):
     
     data.context = data.apply(lambda data: insert_spans(data.context, 
                                                         data.answer_start, 
-                                                        data.answer),
+                                                        data.answer,
+                                                        span_left, 
+                                                        span_right),
                                             axis=1)
-    data.answer_start += 3
+    data.answer_start += len(span_left)
      
     return data
