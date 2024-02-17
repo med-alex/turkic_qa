9fbd1d540da907bf92bbeaafc8eac4edb80f04c0
Alexandr Medovikov <medovikov-aleksandr66@mail.ru>
2024-02-17 16:07:00 +0500

    changing jsonl to json

--- scripts/data_preprocessing/sh_xquad_preprocessing.sh
+++ scripts/data_preprocessing/sh_xquad_preprocessing.sh
@@ -2,4 +2,4 @@
 
 python scripts/data_preprocessing/xquad_preprocessing.py \
     --input_data_path=/home/ml-srv-admin/Projects/turkic_qa/data/source_langs/raw/xquad_en.parquet \
-    --output_data_path=/home/ml-srv-admin/Projects/turkic_qa/data/source_langs/preprocessed/xquad_en.jsonl
+    --output_data_path=/home/ml-srv-admin/Projects/turkic_qa/data/source_langs/preprocessed/xquad_en.json
