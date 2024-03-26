64a32a21097ebfed0a947061af5a6c44792c8a80
Alexandr Medovikov <medovikov-aleksandr66@mail.ru>
2024-03-26 19:31:00 +0500

    remuve checking for int while inserting square brackets, so they will be inserted everywhere

--- scripts/functions/preprocessing.py
+++ scripts/functions/preprocessing.py
@@ -1,14 +1,11 @@
 def insert_spans(text, answer_start_num, suggested_answer, span_left, span_right):
     
-    try: 
-        isinstance(int(suggested_answer), int)
-    except Exception:
-        answer_end_num = answer_start_num + len(suggested_answer)
-        if text[answer_start_num:answer_end_num] == \
-            suggested_answer:
-            text = text[:answer_start_num] + \
-                    span_left + suggested_answer + span_right + \
-                    text[answer_end_num:]
+    answer_end_num = answer_start_num + len(suggested_answer)
+    if text[answer_start_num:answer_end_num] == \
+        suggested_answer:
+        text = text[:answer_start_num] + \
+                span_left + suggested_answer + span_right + \
+                text[answer_end_num:]
                     
     return text
 
