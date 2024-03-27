f70c98f5a218f61f830b1b8b7ed3c2a4872a2469
Alexandr Medovikov <medovikov-aleksandr66@mail.ru>
2024-03-27 23:49:59 +0500

    made xquad preprocessing script universal for any language

--- scripts/data_preprocessing/sh_xquad_preprocessing.sh
+++ scripts/data_preprocessing/sh_xquad_preprocessing.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+lang_tag=$1
+
 python scripts/data_preprocessing/xquad_preprocessing.py \
-    --input_data_path=/home/ml-srv-admin/Projects/turkic_qa/data/source_langs/raw/xquad_en.parquet \
-    --output_data_path=/home/ml-srv-admin/Projects/turkic_qa/data/source_langs/preprocessed/xquad_en.json
+    --input_data_path=/home/ml-srv-admin/Projects/turkic_qa/data/source_langs/raw/xquad_$lang_tag.parquet \
+    --output_data_path=/home/ml-srv-admin/Projects/turkic_qa/data/source_langs/preprocessed/xquad_$lang_tag.json
