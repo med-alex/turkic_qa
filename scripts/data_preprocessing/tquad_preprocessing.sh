8595586d5c027965784597e6bf44cbdc74d4195d
Alexandr Medovikov <medovikov-aleksandr66@mail.ru>
2024-03-31 19:35:57 +0500

    add lang tag for output file name

--- scripts/data_preprocessing/tquad_preprocessing.sh
+++ scripts/data_preprocessing/tquad_preprocessing.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 input_file_name=tquad_$1_tr.json
-output_file_name=tquad_$1.json
+output_file_name=tquad_$1_tr.json
 
 python scripts/data_preprocessing/tquad_preprocessing.py \
     --input_data_path=/home/ml-srv-admin/Projects/turkic_qa/data/source_langs/raw/$input_file_name \
