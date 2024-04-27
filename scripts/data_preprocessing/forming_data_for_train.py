5c03595eafc202ea9f63cea43c88d96595607c1a
Alexandr Medovikov <medovikov-aleksandr66@mail.ru>
2024-04-27 23:50:28 +0500

    rebuild a little bit

--- scripts/data_preprocessing/forming_data_for_train.py
+++ scripts/data_preprocessing/forming_data_for_train.py
@@ -11,16 +11,15 @@ parser.add_argument('--output_dir_path', dest='output_dir_path',
                     type=str, required=True)
 args = parser.parse_args()
 
-data = pd.DataFrame(columns=['context', 'question', 'answer', 'answer_start'])
-
 for source_lang in ['en', 'tr', 'ru']:
     for target_lang in ['kaz', 'uzn']:
         for split in ['test', 'val', 'train']:
+            data = pd.DataFrame(columns=['context', 'question', 'answers'])
             if split in ['val', 'train']:
                 merging_files_paths = []
                 for filename in os.listdir(args.input_dir_path):
                     filename = Path(filename)
-                    if filename.suffix == '.pickle':
+                    if filename.suffix == '.json':
                         name_parts = filename.stem.split('_')
                         
                         file_source_lang = name_parts[1]
@@ -30,19 +29,23 @@ for source_lang in ['en', 'tr', 'ru']:
                         if file_source_lang == source_lang \
                             and file_target_lang == target_lang \
                             and file_split == split:
-
+                            # print(str(Path(args.input_dir_path) / filename))
                             merging_files_paths += [str(Path(args.input_dir_path) / filename)]
             
                 for file_path in merging_files_paths:
-                    data = pd.concat([data, pd.read_pickle(file_path)], ignore_index=True, axis=0)
+                    data = pd.concat([data, pd.read_json(file_path, orient='records', lines=True)], 
+                                     ignore_index=True, axis=0)
                 
+                data.index = [i for i in range(len(data.index))]
                 data = data.sample(frac=1)
-                data.to_pickle(Path(args.output_dir_path) / f'{source_lang}_{target_lang}_{split}.pickle')
+                data['id'] = data.index
+                data.to_json(Path(args.output_dir_path) / f'{source_lang}_{target_lang}_{split}.json',
+                               orient='records', lines=True, force_ascii=False)
 
             elif split == 'test':
                 for filename in os.listdir(args.input_dir_path):
                     filename = Path(filename)
-                    if filename.suffix == '.pickle':
+                    if filename.suffix == '.json':
                         name_parts = filename.stem.split('_')
                         
                         dataset_name = name_parts[0]
@@ -54,6 +57,9 @@ for source_lang in ['en', 'tr', 'ru']:
                             and file_target_lang == target_lang \
                             and file_split == split:
                             
-                            data_as_it_is = pd.read_pickle(str(Path(args.input_dir_path) / filename))
-                            data_as_it_is.sample(frac=1)
-                            data_as_it_is.to_pickle(Path(args.output_dir_path) / f'{source_lang}_{target_lang}_{split}_{dataset_name}.pickle')
+                            data_as_it_is = pd.read_json(str(Path(args.input_dir_path) / filename), orient='records', lines=True)
+                            data_as_it_is = data_as_it_is.sample(frac=1)
+                            data_as_it_is['id'] = data_as_it_is.index
+                            # print(str(Path(args.output_dir_path) / f'{source_lang}_{target_lang}_{split}_{dataset_name}.json'))
+                            data_as_it_is.to_json(Path(args.output_dir_path) / f'{source_lang}_{target_lang}_{split}_{dataset_name}.json',
+                                                    orient='records', lines=True, force_ascii=False)
